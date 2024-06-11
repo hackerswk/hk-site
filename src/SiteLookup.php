738355180720a8af49bd083703fb79d73303284f
wu gong Sie <swookon@gmail.com>
2024-06-11 15:30:53 +0800

    Adjust logic

--- src/SiteLookup.php
+++ src/SiteLookup.php
@@ -79,27 +79,27 @@ class SiteLookup
     /**
      * Check if site_id exists in site-lookup.php.
      *
-     * @param int $site_id The ID of the site to check.
+     * @param int $site_code The site code of the site to check.
      * @param string $path The path to the directory containing the config files.
      * @return bool True if the site_id exists, false otherwise.
      */
-    public function isSiteIdInSiteLookup($site_id, $path)
+    public function isSiteCodeInSiteLookup($siteCode, $path)
     {
         $site_lookup = $this->getSiteLookup($path, 0); // 0 indicates site-lookup.php
-        return isset($site_lookup[$site_id]);
+        return isset($site_lookup[$siteCode]);
     }
 
     /**
      * Check if site_id exists in domain-lookup.php.
      *
-     * @param int $site_id The ID of the site to check.
+     * @param int $domain The domain of the site to check.
      * @param string $path The path to the directory containing the config files.
      * @return bool True if the site_id exists, false otherwise.
      */
-    public function isSiteIdInDomainLookup($site_id, $path)
+    public function isDomainInDomainLookup($domain, $path)
     {
         $domain_lookup = $this->getSiteLookup($path, 1); // 1 indicates domain-lookup.php
-        return isset($domain_lookup[$site_id]);
+        return isset($domain_lookup[$domain]);
     }
 
     /**
@@ -133,8 +133,11 @@ class SiteLookup
             // Read existing configuration
             $existingConfig = file_exists($config_file) ? $configHandler->readConfig() : [];
 
-            // Add or update the site configuration
-            $existingConfig[$site_id] = $data;
+            // Determine the key based on whether it's a domain or site lookup
+            $key = $is_domain ? $data['domain'] : $data['site_code'];
+
+            // Add or update the site configuration using the appropriate key
+            $existingConfig[$key] = $data;
 
             // Write the updated configuration
             if ($configHandler->generateConfig($existingConfig)) {
@@ -145,6 +148,7 @@ class SiteLookup
         }
         return false;
     }
+
 
     /**
      * Get the configuration file path.
@@ -175,43 +179,71 @@ class SiteLookup
             $site = new Site($this->pdo);
             $public_sites = $site->getPublicSites();
             $data = [];
+
             foreach ($public_sites as $site_data) {
-                $data[$site_data['site_id']] = [
-                    'site_id' => $site_data['site_id'],
-                    'site_code' => $site_data['site_code'],
-                    'domain' => $site_data['domain'],
-                    'file_path' => $site_data['file_path'],
-                ];
+                if ($is_domain) {
+                    // If is_domain is true, use the domain as the key
+                    $data[$site_data['domain']] = [
+                        'site_id' => $site_data['site_id'],
+                        'site_code' => $site_data['site_code'],
+                        'domain' => $site_data['domain'],
+                        'file_path' => $site_data['file_path'],
+                    ];
+                } else {
+                    // If is_domain is false, use the site_code as the key
+                    $data[$site_data['site_code']] = [
+                        'site_id' => $site_data['site_id'],
+                        'site_code' => $site_data['site_code'],
+                        'domain' => $site_data['domain'],
+                        'file_path' => $site_data['file_path'],
+                    ];
+                }
             }
+
             $configHandler = new PhpConfigHandler($config_file);
             $configHandler->generateConfig($data);
         }
     }
 
+
     /**
      * Remove site from lookup configuration.
      *
-     * @param int $site_id The ID of the site to remove.
+     * @param string $key The key (domain or site_code) of the site to remove.
      * @param string $path The base path to the directory containing the config files.
      * @param int $is_domain Flag indicating if the site is represented by a domain.
      * @return bool True if the site is successfully removed, false otherwise.
      */
-    public function removeSiteFromLookup($site_id, $path, $is_domain)
+    public function removeSiteFromLookup($key, $path, $is_domain)
     {
         $config_file = $this->getConfigFilePath($path, $is_domain);
         if (file_exists($config_file)) {
             $configHandler = new PhpConfigHandler($config_file);
             $existingConfig = $configHandler->readConfig();
 
-            if (isset($existingConfig[$site_id])) {
-                unset($existingConfig[$site_id]);
+            if ($is_domain) {
+                // If it's a domain lookup, check if the key (domain) exists
+                if (isset($existingConfig[$key])) {
+                    unset($existingConfig[$key]);
 
-                // Write the updated configuration
-                if ($configHandler->generateConfig($existingConfig)) {
-                    return true;
+                    // Write the updated configuration
+                    if ($configHandler->generateConfig($existingConfig)) {
+                        return true;
+                    }
+                }
+            } else {
+                // If it's a site lookup, check if the key (site_code) exists
+                if (isset($existingConfig[$key])) {
+                    unset($existingConfig[$key]);
+
+                    // Write the updated configuration
+                    if ($configHandler->generateConfig($existingConfig)) {
+                        return true;
+                    }
                 }
             }
         }
         return false;
     }
+
 }
